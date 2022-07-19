ed2a86cbf272d7d7bcf33db24afa69ea0233eac6
kamui-7 <gabrielmarinsrodrigues@gmail.com>
2022-07-19 17:31:29 -0300

    Exercicios Lista 6 - Classe

---
+++ ClasseAbst/Animais/Animal.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClasseAbst.Animais
+{
+    public class Animal
+    {
+        string Nome { get; set; }
+
+        int Patas { get; set; }
+
+        Categoria Tipo { get; set; }
+
+        int Quantidade { get; set; }
+
+        public Animal(string nome, int patas, Categoria tipo, int quantidade)
+        {
+            Nome = nome;
+            Patas = patas;
+            Tipo = tipo;
+            Quantidade = quantidade;
+        }
+
+        public override string ToString()
+        {
+            return
+                $"\n Nome do animal: {Nome}" +
+                $"\n Quantas patas esse animal tem: {Patas}" +
+                $"\n Qual é a classificação desse animal: {Tipo}" +
+                $"\n Quantos animais deste temos em casa: {Quantidade}";
+        }
+    }
+    public enum Categoria
+    {
+        MAMIFERO,
+        REPTIL,
+        ANFIBIO,
+        AVE,
+
+    }
+        
+
+}
---
+++ ClasseAbst/Program.cs
@@ -0,0 +1,23 @@
+﻿using ClasseAbst.Animais;
+using System;
+
+namespace ClasseAbst
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal Cachorro = new Animal("Cachorro", 4, Categoria.MAMIFERO, 4);
+            Animal Gato = new Animal("Gato", 4, Categoria.MAMIFERO, 3);
+            Animal Papagaio = new Animal("Papagaio", 2, Categoria.AVE, 2);
+            Animal Cobra = new Animal("Cobra", 0, Categoria.REPTIL, 0);
+
+            Console.WriteLine(Cachorro.ToString());
+            Console.WriteLine(Gato.ToString());
+            Console.WriteLine(Papagaio.ToString());
+            Console.WriteLine(Cobra.ToString());
+
+
+        }
+    }
+}
